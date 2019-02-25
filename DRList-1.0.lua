70359ffc1e95cb0a2cd3bcc9c2e0b69d5e331710
Thomas Pedersen <thomaszrx@gmail.com>
2019-02-25 18:02:45 +0100

    set locale import to do nothing if a sentence haven't been translated yet

--- DRList-1.0.lua
+++ DRList-1.0.lua
@@ -41,21 +41,21 @@ L["CHEAP_SHOT"] = GetSpellInfo(1833)
 -- luacheck: push ignore 542
 local locale = GetLocale()
 if locale == "deDE" then
-    --@localization(locale="deDE", format="lua_additive_table")@
+    --@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
 elseif locale == "frFR" then
-    --@localization(locale="frFR", format="lua_additive_table")@
+    --@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore")@
 elseif locale == "itIT" then
-    --@localization(locale="itIT", format="lua_additive_table")@
+    --@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="ignore")@
 elseif locale == "koKR" then
-    --@localization(locale="koKR", format="lua_additive_table")@
+    --@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore")@
 elseif locale == "ptBR" then
-    --@localization(locale="ptBR", format="lua_additive_table")@
+    --@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="ignore")@
 elseif locale == "ruRU" then
-    --@localization(locale="ruRU", format="lua_additive_table")@
+    --@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="ignore")@
 elseif locale == "esES" or locale == "esMX" then
-    --@localization(locale="esES", format="lua_additive_table")@
+    --@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore")@
 elseif locale == "zhCN" or locale == "zhTW" then
-    --@localization(locale="zhCN", format="lua_additive_table")@
+    --@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 -- luacheck: pop
 -------------------------------------------------------------------------------

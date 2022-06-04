056560a13687da60113dbd2a4c9d46ec316c994c
wardz <10900212+wardz@users.noreply.github.com>
2022-06-04 09:28:20 +0200

    bump spelllist version

--- DRList-1.0/Spells.lua
+++ DRList-1.0/Spells.lua
@@ -1,5 +1,5 @@
 local Lib, version = LibStub("DRList-1.0")
-if Lib.spellList and version >= 29 then return end
+if Lib.spellList and version >= 30 then return end
 
 if Lib.gameExpansion == "retail" then
 
